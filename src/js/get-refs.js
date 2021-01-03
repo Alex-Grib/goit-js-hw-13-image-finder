cbca2eb4665895f85c03a72a8ab515e4cba50424
Alexandr Grib <alexgribaldi@gmail.com>
2021-01-03 16:22:01 +0100

    дз 13

---
+++ src/js/get-refs.js
@@ -0,0 +1,8 @@
+
+export default function getRefs() {
+    return {
+      galleryContainer: document.querySelector('#gallery'),
+      form: document.querySelector('#search-form'),
+      header: document.querySelector('.search'),
+    };
+  }
