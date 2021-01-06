9ae541548891f7a2c027e814e780c14953dc27a6
Alexandr Grib <alexgribaldi@gmail.com>
2021-01-06 19:09:09 +0100

    ДЗ 13

--- src/js/get-refs.js
+++
@@ -1,8 +0,0 @@
-
-export default function getRefs() {
-    return {
-      galleryContainer: document.querySelector('#gallery'),
-      form: document.querySelector('#search-form'),
-      header: document.querySelector('.search'),
-    };
-  }
